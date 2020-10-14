5c4cc9843fd64bfe10f299e4084263a00cd73fb5
suman <77spartann@gmail.com>
2020-10-14 21:42:07 +0530

    Fotonicia Assignment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from "axios";
+import Banner from "./Component/Banner";
+import Spinner from 'react-bootstrap/Spinner'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      
+      obj:[],
+      loading:true
+     
+    }
 }
 
-export default App;
+  componentDidMount(){
+    axios.get('https://jsonware.com/json/688410e8f4013fbe56009d8e1621f2a4.json').then(res=>{
+      console.log(res.data.MyData);
+      this.setState({
+        obj:res.data.MyData,loading:false
+
+      })
+      console.log(this.state.obj);
+    }).catch(err=>{
+      console.log("Something went wrong");
+    })
+     
+     //console.log(this.state.listItem[0]);
+
+  }
+  render() {
+    if(this.state.loading){
+      return(
+        <p className="h2 text-center mt-5">Loading....  <Spinner animation="border" /></p>
+      )
+    }
+    return (
+     <>
+       {
+         this.state.obj.map((value,index)=>
+         {
+           return <div className="container">
+             <div className="text-center btn btn-lg btn-primary btn-block mt-2 mb-5"><strong>{value.headerTitle}</strong></div>
+             <div className="row">
+               {value.listItem.map((val,idx)=>{
+                 return  <div className="col-sm-4  text-center col-md-2 col col-lg--2 mx-auto">
+                   <figure>
+                 <img style={{height:"200px",width:"200px"}} src={val.image}></img>
+               <figcaption className="text-center mt-1 "><strong>{val.name}</strong></figcaption>
+                 </figure>
+               </div>
+
+               })}
+               <span>{(index+1)%2==0?<Banner /> :""}</span>
+        
+             </div>
+             
+             
+           </div>
+
+         })
+       }
+       <div className="container"> 
+       <div className="row"><Banner/></div>
+       </div>
+     
+      </>
+    )
+  }
+}
+
+export default App
---
+++ src/Component/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Banner() {
+    return (
+        <>
+      <div  className=" col col-12 mx-auto mt-3 mb-1">
+          <img style={{height:"100px",width:"100%"}}src="https://www.publicdomainpictures.net/pictures/150000/velka/banner-header-tapete-145002399028x.jpg" alt="Banner"></img>
+
+      </div>
+      </>
+    )
+}
